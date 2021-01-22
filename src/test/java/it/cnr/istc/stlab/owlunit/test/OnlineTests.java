cc3fb5d402b4946cdb6609e11cc2f0bb5a38108d
Luigi Asprino <luigi.asprino@gmail.com>
2021-01-22 19:03:04 +0100

    test suite test

--- src/test/java/it/cnr/istc/stlab/owlunit/test/OnlineTests.java
+++ src/test/java/it/cnr/istc/stlab/owlunit/test/OnlineTests.java
@@ -1,5 +1,6 @@
 package it.cnr.istc.stlab.owlunit.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -8,6 +9,7 @@ import it.cnr.istc.stlab.owlunit.workers.CompetencyQuestionVerificationExecutor;
 import it.cnr.istc.stlab.owlunit.workers.ErrorProvocationTestExecutor;
 import it.cnr.istc.stlab.owlunit.workers.InferenceVerificationTestExecutor;
 import it.cnr.istc.stlab.owlunit.workers.OWLUnitException;
+import it.cnr.istc.stlab.owlunit.workers.TestSuiteExecutor;
 
 public class OnlineTests {
 
@@ -21,7 +23,7 @@ public class OnlineTests {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void test2() {
 		String URItest = "https://raw.githubusercontent.com/luigi-asprino/owl-unit/main/examples/cq1-testalod.ttl";
@@ -32,7 +34,7 @@ public class OnlineTests {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void test3() {
 		String URItest = "https://raw.githubusercontent.com/luigi-asprino/owl-unit/main/examples/iv.ttl";
@@ -43,8 +45,7 @@ public class OnlineTests {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	@Test
 	public void test4() {
 		String URItest = "https://raw.githubusercontent.com/luigi-asprino/owl-unit/main/examples/ep.ttl";
@@ -55,16 +56,12 @@ public class OnlineTests {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void test5() {
-		String URItest = "https://raw.githubusercontent.com/luigi-asprino/owl-unit/main/examples/ep.ttl";
-		ErrorProvocationTestExecutor te = new ErrorProvocationTestExecutor(URItest);
-		try {
-			assertTrue(te.runTest());
-		} catch (OWLUnitException e) {
-			e.printStackTrace();
-		}
+		String URItest = "https://raw.githubusercontent.com/luigi-asprino/owl-unit/main/examples/suite1.ttl";
+		TestSuiteExecutor te = new TestSuiteExecutor(URItest);
+		assertEquals(3, te.runTestSuite());
 	}
 
 }
